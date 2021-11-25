3c461d1d0624686811c3f4d39160b83987616654
kevinhadinatatan <kevinhadinataonly@gmail.com>
2021-11-25 11:25:01 +0700

    penambahan credential type admin

--- src/main/java/com/fundraisey/backend/repository/startup/CredentialTypeRepository.java
+++ src/main/java/com/fundraisey/backend/repository/startup/CredentialTypeRepository.java
@@ -1,7 +1,7 @@
 package com.fundraisey.backend.repository.startup;
 
 import com.fundraisey.backend.entity.startup.CredentialType;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CredentialTypeRepository extends PagingAndSortingRepository<CredentialType, Long> {
+public interface CredentialTypeRepository extends JpaRepository<CredentialType, Long> {
 }
