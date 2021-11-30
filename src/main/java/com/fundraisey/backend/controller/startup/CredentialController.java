e6d8adda658b4d2063583581271a0152f2c43323
kevinhadinatatan <kevinhadinataonly@gmail.com>
2021-11-30 19:35:23 +0700

    insert credential controller

--- src/main/java/com/fundraisey/backend/controller/startup/CredentialController.java
+++ src/main/java/com/fundraisey/backend/controller/startup/CredentialController.java
@@ -1,23 +1,41 @@
 package com.fundraisey.backend.controller.startup;
 
+import com.fundraisey.backend.model.startup.CredentialModel;
+import com.fundraisey.backend.service.UserService;
+import com.fundraisey.backend.service.startup.CredentialService;
+import com.fundraisey.backend.service.startup.ProductService;
 import com.fundraisey.backend.util.ResponseTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.security.Principal;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/startup")
 public class CredentialController {
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    CredentialService credentialService;
+
     ResponseTemplate responseTemplate = new ResponseTemplate();
 
     @Secured("ROLE_STARTUP")
     @PostMapping("/credential/add")
-    ResponseEntity<Map> insertLoan() {
-        return null;
+    ResponseEntity<Map> insert(@Valid @RequestBody CredentialModel credentialModel, Principal principal) {
+        Long id = (principal == null) ? null : userService.getUserById(principal);
+
+        Map response = credentialService.insert(credentialModel, id);
+
+        return responseTemplate.controllerHttpRestResponse(response);
     }
 
     @Secured("ROLE_STARTUP")
