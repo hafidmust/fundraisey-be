7349ee50aa6a3be3d0ec0810fd481412d9ab0665
kevinhadinatatan <kevinhadinataonly@gmail.com>
2021-12-13 20:32:20 +0700

    add unit test GET postman update

--- src/main/java/com/fundraisey/backend/unittest/startup/StartupControllerTest.java
+++ src/main/java/com/fundraisey/backend/unittest/startup/StartupControllerTest.java
@@ -1,20 +1,13 @@
 package com.fundraisey.backend.unittest.startup;
 
-import com.fundraisey.backend.entity.auth.User;
-import com.fundraisey.backend.repository.auth.UserRepository;
 import com.fundraisey.backend.unittest.UnitTest;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import javax.print.attribute.standard.Media;
 import java.security.Principal;
 
 import static org.junit.Assert.assertEquals;
@@ -24,6 +17,14 @@ public class StartupControllerTest extends UnitTest {
     @MockBean
     Principal principal;
 
+    String body = "{\n" +
+            "    \"email\": \"startup@fundraisey.com\",\n" +
+            "    \"password\": \"password\",\n" +
+            "    \"grant_type\": \"password\",\n" +
+            "    \"client_id\": \"client-web\",\n" +
+            "    \"client_secret\": \"password\"\n" +
+            "}";
+
     @Override
     @Before
     public void setUp() {
@@ -32,17 +33,8 @@ public class StartupControllerTest extends UnitTest {
 
     @Test
     public void getStartupProfile() throws Exception {
-
-        String body = "{\n" +
-                "    \"email\": \"startup@fundraisey.com\",\n" +
-                "    \"password\": \"password\",\n" +
-                "    \"grant_type\": \"password\",\n" +
-                "    \"client_id\": \"client-web\",\n" +
-                "    \"client_secret\": \"password\"\n" +
-                "}";
-
         MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/v1/login")
-                .contentType(MediaType.APPLICATION_JSON_VALUE).content(body)
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(this.body)
         ).andReturn();
 
         String response = result.getResponse().getContentAsString();
@@ -53,6 +45,216 @@ public class StartupControllerTest extends UnitTest {
 //
         MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
                 .header("Authorization", "Bearer " + token)
+                .principal(principal)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        System.out.println("Response = ");
+        System.out.println(content);
+    }
+
+    @Test
+    public void getStartupIndex() throws Exception {
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/v1/login")
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(this.body)
+        ).andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        response = response.replace("{\"data\":{\"access_token\":\"", "");
+        String token = response.replace("\",\"is_enabled\":true,\"roles\":[\"ROLE_STARTUP\"]},\"message\":\"success\",\"status\":200}", "");
+
+        String uri = "/v1/startup/index";
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .header("Authorization", "Bearer " + token)
+                .principal(principal)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        System.out.println("Response = ");
+        System.out.println(content);
+    }
+
+    @Test
+    public void getLoan() throws Exception {
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/v1/login")
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(this.body)
+        ).andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        response = response.replace("{\"data\":{\"access_token\":\"", "");
+        String token = response.replace("\",\"is_enabled\":true,\"roles\":[\"ROLE_STARTUP\"]},\"message\":\"success\",\"status\":200}", "");
+
+        String uri = "/v1/loan/startup";
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .header("Authorization", "Bearer " + token)
+                .param("page", "0")
+                .param("size", "20")
+                .param("sort-by", "status")
+                .param("sort-type", "desc")
+                .param("search", "test")
+                .principal(principal)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        System.out.println("Response = ");
+        System.out.println(content);
+    }
+
+    @Test
+    public void getPaymentList() throws Exception {
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/v1/login")
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(this.body)
+        ).andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        response = response.replace("{\"data\":{\"access_token\":\"", "");
+        String token = response.replace("\",\"is_enabled\":true,\"roles\":[\"ROLE_STARTUP\"]},\"message\":\"success\",\"status\":200}", "");
+
+        String uri = "/v1/startup/loan/1/payment-list";
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .header("Authorization", "Bearer " + token)
+                .principal(principal)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        System.out.println("Response = ");
+        System.out.println(content);
+    }
+
+    @Test
+    public void getNotification() throws Exception {
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/v1/login")
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(this.body)
+        ).andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        response = response.replace("{\"data\":{\"access_token\":\"", "");
+        String token = response.replace("\",\"is_enabled\":true,\"roles\":[\"ROLE_STARTUP\"]},\"message\":\"success\",\"status\":200}", "");
+
+        String uri = "/v1/startup/notification/all";
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .header("Authorization", "Bearer " + token)
+                .param("page", "0")
+                .param("size", "20")
+                .param("sort-by", "id")
+                .param("sort-type", "desc")
+                .principal(principal)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        System.out.println("Response = ");
+        System.out.println(content);
+    }
+
+    @Test
+    public void getWithdrawHistory() throws Exception {
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/v1/login")
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(this.body)
+        ).andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        response = response.replace("{\"data\":{\"access_token\":\"", "");
+        String token = response.replace("\",\"is_enabled\":true,\"roles\":[\"ROLE_STARTUP\"]},\"message\":\"success\",\"status\":200}", "");
+
+        String uri = "/v1/startup/loan/withdraw-history";
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .header("Authorization", "Bearer " + token)
+                .principal(principal)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        System.out.println("Response = ");
+        System.out.println(content);
+    }
+
+    @Test
+    public void getBankListForLoan() throws Exception {
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/v1/login")
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(this.body)
+        ).andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        response = response.replace("{\"data\":{\"access_token\":\"", "");
+        String token = response.replace("\",\"is_enabled\":true,\"roles\":[\"ROLE_STARTUP\"]},\"message\":\"success\",\"status\":200}", "");
+
+        String uri = "/v1/startup/loan/withdraw/bank-list";
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .header("Authorization", "Bearer " + token)
+                .principal(principal)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        System.out.println("Response = ");
+        System.out.println(content);
+    }
+
+    @Test
+    public void getLoanDetail() throws Exception {
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/v1/login")
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(this.body)
+        ).andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        response = response.replace("{\"data\":{\"access_token\":\"", "");
+        String token = response.replace("\",\"is_enabled\":true,\"roles\":[\"ROLE_STARTUP\"]},\"message\":\"success\",\"status\":200}", "");
+
+        String uri = "/v1/loan/detail/1";
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .header("Authorization", "Bearer " + token)
+                .principal(principal)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        System.out.println("Response = ");
+        System.out.println(content);
+    }
+
+    @Test
+    public void getPaymentDetail() throws Exception {
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.post("/v1/login")
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(this.body)
+        ).andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        response = response.replace("{\"data\":{\"access_token\":\"", "");
+        String token = response.replace("\",\"is_enabled\":true,\"roles\":[\"ROLE_STARTUP\"]},\"message\":\"success\",\"status\":200}", "");
+
+        String uri = "/v1/startup/loan/1/payment";
+
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .header("Authorization", "Bearer " + token)
+                .param("period", "1")
                 .principal(principal)
                 .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 
